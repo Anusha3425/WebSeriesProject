9ec6588cef516c2024fcac8924903864f5f5a23d
Anusha3425 <anusha.k@cumulations.com>
2024-08-21 18:53:52 +0530

    FetchAPI

---
+++ index.js
@@ -0,0 +1,75 @@
+
+function fetchSeries(){
+    fetch(`http://localhost:3000/series`)
+    .then(resp=>resp.json())
+    .then(async data => {
+        console.log(data,'data one'); 
+        if(data){
+            showCatalogue(data)
+
+        }
+    })
+   
+}
+
+const seriesCollection=document.getElementById("series-collection");
+async function showCatalogue(series){
+    seriesCollection.innerHTML=""
+    console.log(series,'test one')
+let data = series
+data?.forEach((i)=>{
+        seriesCollection.innerHTML+=`
+        <div id="card" data-id=${i.data}>
+        <h2>${i.name}</h2>
+        <img src="${i.image}"
+        </div>`
+    });
+    console.log("Success")
+}
+
+fetchSeries();
+
+const addSeries=document.querySelector(".add-series");
+addSeries.addEventListener('submit', function(event){
+    fetch('http://localhost:3000/series', {
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify({
+            name: event.target.name.value,
+            image: `${event.target.image.value}`
+        })
+    })
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('response was not ok');
+        }
+        return res.json();
+    })
+    .then(data => {
+        showCatalogue(data);
+    })
+});
+
+// addSeries.addEventListener(onclick, function(event){
+    function update(){fetch('http://localhost:3000/series/b225', {
+        method:'PUT',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({
+            name:event.target.name.value,
+        })
+    })
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('response was not ok');
+        }
+        return res.json();
+    })
+    .then(data => {
+        showCatalogue(data);
+    })
+
+    }
